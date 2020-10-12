df02da043ae57488e487d6bfee4fd34025dc2440
benitolsantos <github email address>
2020-10-11 23:46:41 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+//Create a constructor function for each of the following:
+
+//we'll call it a factory function because it spits out new instances
+
+//Scooter with year, color, and model properties
+function Scooter(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+//Driver with name, age, and experience properties
+function Driver(name, age, experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+//PickupLocation with address and city properties
+function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+}
